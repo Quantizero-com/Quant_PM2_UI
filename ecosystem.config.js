c069847efb3bad8d21c119724f723fd548b86d98
teamsoftvnofficial <quangthinh.dico@gmail.com>
2024-12-31 10:03:14 +0700

    update ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Quant_PM2_UI',
+      script: 'node',
+      args: '.',
+      instances: 1,                              // Number of instances (1 for a single instance)
+      exec_mode: 'fork',                         // Single process mode (use 'cluster' for multiple CPU cores)
+      env: {
+        NODE_ENV: 'production',                 // Environment variable (production)
+      },
+    },
+  ],
+};
